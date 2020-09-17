ed70a7ad358e9db1af5d9bdbded58dc9cb458ccc
Michał Lemke <lemke.michal@gmail.com>
2020-09-17 13:22:21 +0200

    remove unused apps; add asdf plugins
    Signed-off-by: Michał Lemke <lemke.michal@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,3 +25,16 @@ ansible-playbook playbook.yml --ask-become-pass
 
 # install fzf bindings
 $(brew --prefix)/opt/fzf/install
+
+# asdf plugins
+asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git
+asdf install terraform latest:0.11
+asdf install terraform latest:0.12
+asdf install terraform latest:0.13
+
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
+asdf install rust latest
+
+asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
+asdf install helm latest:2
+asdf install helm latest:3
